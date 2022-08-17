92ba9853e252289a3292a7b9da365413c230f491
666Deadsoul <102571526+666Deadsoul@users.noreply.github.com>
2022-08-17 09:01:36 +0545

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Compare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a==b)
+        {
+            System.out.println("They are equal.");
+        }
+        else if(a>b)
+        {
+            System.out.println(a +" is greater than " + b);
+        }
+        else {
+            System.out.println(b + " is greater than " + a);
+        }
+
+    }
+}
